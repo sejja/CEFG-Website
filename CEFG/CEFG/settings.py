c42cf083f2f8e9d4da7874876bbdaf1ba366d08a
Diego Revilla <diego.revilla.r@gmail.com>
2025-12-03 18:08:13 +0100

    appended statics

--- CEFG/CEFG/settings.py
+++ CEFG/CEFG/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
